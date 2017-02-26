d7084e7bfc478cd82ab8a9d0e5c7452c18073298
Preethi Vaidyanathan <preethi@Preethis-MacBook-Pro.local>
2017-02-26 17:52:52 -0500

    Reformatted code and split into methods.

--- src/lipsync_generator.py
+++ src/lipsync_generator.py
@@ -2,89 +2,117 @@ import os
 import tempfile
 import speech_recognition
 
-audio_file = 'hello_world.wav'
 
-curr_dir = os.getcwd()
-model_dir = '/Library/Python/2.7/site-packages/pocketsphinx/model'
-data_dir = os.path.join(curr_dir, '../data/')
-audio_data_path = os.path.join(data_dir, audio_file)
+class LipsyncGenerator:
+    def __init__(self):
+        pass
 
-speech_recognition_directory = '/Library/Python/2.7/site-packages/speech_recognition/'
+    def generate_timestamp_script_list(self):
 
-language = "en-US"
-keyword_entries = None
-#assert isinstance(audio_data_path, AudioData), "``audio_data_path`` must be audio data"
-assert isinstance(language, str), "``language`` must be a string"
-assert keyword_entries is None or all(
-    isinstance(keyword, (type(""), type(u""))) and 0 <= sensitivity <= 1 for keyword, sensitivity in
-    keyword_entries), "``keyword_entries`` must be ``None`` or a list of pairs of strings and numbers between 0 and 1"
+        audio_file = 'TRLT.wav'
+        # explicitly specified set of keywords
+        keyword_entries = None
+        audio_data_path, decoder = self._setup_decoder(audio_file, keyword_entries)
 
-# import the PocketSphinx speech recognition module
-try:
-    from pocketsphinx import pocketsphinx
-except ImportError:
-    raise speech_recognition.RequestError("missing PocketSphinx module: ensure that PocketSphinx is set up correctly.")
-except ValueError:
-    raise speech_recognition.RequestError(
-        "bad PocketSphinx installation detected; make sure you have PocketSphinx version 0.0.9 or better.")
+        # obtain audio data
+        r = speech_recognition.Recognizer()
+        with speech_recognition.AudioFile(audio_data_path) as source:
+            audio_data = r.record(source)
+        # the included language models require audio to be 16-bit mono 16 kHz in little-endian format
+        raw_data = audio_data.get_raw_data(convert_rate=16000, convert_width=2)
 
-language_directory = os.path.join(os.path.dirname(speech_recognition_directory), "pocketsphinx-data", language)
-if not os.path.isdir(language_directory):
-    raise speech_recognition.RequestError("missing PocketSphinx language data directory: \"{}\"".format(language_directory))
-acoustic_parameters_directory = os.path.join(language_directory, "acoustic-model")
-if not os.path.isdir(acoustic_parameters_directory):
-    raise speech_recognition.RequestError(
-        "missing PocketSphinx language model parameters directory: \"{}\"".format(acoustic_parameters_directory))
-language_model_file = os.path.join(language_directory, "language-model.lm.bin")
-if not os.path.isfile(language_model_file):
-    raise speech_recognition.RequestError("missing PocketSphinx language model file: \"{}\"".format(language_model_file))
-phoneme_dictionary_file = os.path.join(language_directory, "pronounciation-dictionary.dict")
-if not os.path.isfile(phoneme_dictionary_file):
-    raise speech_recognition.RequestError("missing PocketSphinx phoneme dictionary file: \"{}\"".format(phoneme_dictionary_file))
+        # obtain recognition results
+        if keyword_entries is not None:
+            with tempfile.NamedTemporaryFile("w") as f:
+                # generate a keywords file - Sphinx documentation recommendeds sensitivities between 1e-50 and 1e-5
+                f.writelines("{} /1e{}/\n".format(keyword, 100 * sensitivity - 110) for keyword, sensitivity in keyword_entries)
+                f.flush()
 
-# create decoder object
-config = pocketsphinx.Decoder.default_config()
-config.set_string("-hmm",
-                  acoustic_parameters_directory)  # set the path of the hidden Markov model (HMM) parameter files
-config.set_string("-lm", language_model_file)
-config.set_string("-dict", phoneme_dictionary_file)
-config.set_string("-logfn", os.devnull)  # disable logging (logging causes unwanted output in terminal)
-decoder = pocketsphinx.Decoder(config)
+                # perform the speech recognition with the keywords file (this is inside the context manager so the file
+                # isn't deleted until we're done)
+                decoder.set_kws("keywords", f.name)
+                decoder.set_search("keywords")
+                # begin utterance processing
+                decoder.start_utt()
+                # process audio data with recognition enabled (no_search = False), as a full utterance (full_utt = True)
+                decoder.process_raw(raw_data, False, True)
+                # stop utterance processing
+                decoder.end_utt()
+        # no keywords, perform freeform recognition
+        else:
+            # begin utterance processing
+            decoder.start_utt()
+            # process audio data with recognition enabled (no_search = False), as a full utterance (full_utt = True)
+            decoder.process_raw(raw_data, False, True)
+            # stop utterance processing
+            decoder.end_utt()
 
-# obtain audio data
-r = speech_recognition.Recognizer()
-with speech_recognition.AudioFile(audio_data_path) as source:
-    audio_data = r.record(source)
-raw_data = audio_data.get_raw_data(convert_rate=16000,
-                                   convert_width=2)  # the included language models require audio to be 16-bit mono 16 kHz in little-endian format
+        script_list = [(seg.word, seg.start_frame, seg.end_frame) for seg in decoder.seg()]
+        line_to_print = ''
+        for word in script_list:
+            line_to_print = line_to_print + word[0]
+        print('Words I heard: ', line_to_print)
+        return script_list
 
-# obtain recognition results
-if keyword_entries is not None:  # explicitly specified set of keywords
-    with tempfile.NamedTemporaryFile("w") as f:
-        # generate a keywords file - Sphinx documentation recommendeds sensitivities between 1e-50 and 1e-5
-        f.writelines("{} /1e{}/\n".format(keyword, 100 * sensitivity - 110) for keyword, sensitivity in keyword_entries)
-        f.flush()
+    @staticmethod
+    def _setup_decoder(audio_file, keyword_entries):
 
-        # perform the speech recognition with the keywords file (this is inside the context manager so the file isn;t deleted until we're done)
-        decoder.set_kws("keywords", f.name)
-        decoder.set_search("keywords")
-        decoder.start_utt()  # begin utterance processing
-        decoder.process_raw(raw_data, False,
-                            True)  # process audio data with recognition enabled (no_search = False), as a full utterance (full_utt = True)
-        decoder.end_utt()  # stop utterance processing
-else:  # no keywords, perform freeform recognition
-    decoder.start_utt()  # begin utterance processing
-    decoder.process_raw(raw_data, False,
-                        True)  # process audio data with recognition enabled (no_search = False), as a full utterance (full_utt = True)
-    decoder.end_utt()  # stop utterance processing
+        language = "en-US"
 
-print('Best hypothesis segments:', [(seg.word, seg.start_frame, seg.end_frame) for seg in decoder.seg()])
+        audio_file_type = audio_file.split(".")[1]
 
-# return results
-hypothesis = decoder.hyp()
-if hypothesis is not None:
-    sr_hypothesis = hypothesis.hypstr
-    print('Speech I recognized: ' + sr_hypothesis)
-else:
-    raise speech_recognition.UnknownValueError()  # no transcriptions available
+        if audio_file_type == 'wav':
+            curr_dir = os.getcwd()
+            data_dir = os.path.join(curr_dir, '../data/')
+            speech_recognition_directory = '/Library/Python/2.7/site-packages/speech_recognition/'
+            audio_data_path = os.path.join(data_dir, audio_file)
+        else:
+            raise speech_recognition.RequestError("file type must be .wav")
 
+        assert isinstance(language, str), "``language`` must be a string"
+        assert keyword_entries is None or all(
+            isinstance(keyword, (type(""), type(u""))) and 0 <= sensitivity <= 1 for keyword, sensitivity in
+            keyword_entries), "``keyword_entries`` must be ``None`` or a list of pairs of strings and numbers between 0 and 1"
+        # import the PocketSphinx speech recognition module
+        try:
+            from pocketsphinx import pocketsphinx
+        except ImportError:
+            raise speech_recognition.RequestError(
+                "missing PocketSphinx module: ensure that PocketSphinx is set up correctly.")
+        except ValueError:
+            raise speech_recognition.RequestError(
+                "bad PocketSphinx installation detected; make sure you have PocketSphinx version 0.0.9 or better.")
+
+        language_directory = os.path.join(os.path.dirname(speech_recognition_directory), "pocketsphinx-data", language)
+        if not os.path.isdir(language_directory):
+            raise speech_recognition.RequestError(
+                "missing PocketSphinx language data directory: \"{}\"".format(language_directory))
+        acoustic_parameters_directory = os.path.join(language_directory, "acoustic-model")
+        if not os.path.isdir(acoustic_parameters_directory):
+            raise speech_recognition.RequestError(
+                "missing PocketSphinx language model parameters directory: \"{}\"".format(
+                    acoustic_parameters_directory))
+        language_model_file = os.path.join(language_directory, "language-model.lm.bin")
+        if not os.path.isfile(language_model_file):
+            raise speech_recognition.RequestError(
+                "missing PocketSphinx language model file: \"{}\"".format(language_model_file))
+        phoneme_dictionary_file = os.path.join(language_directory, "pronounciation-dictionary.dict")
+        if not os.path.isfile(phoneme_dictionary_file):
+            raise speech_recognition.RequestError(
+                "missing PocketSphinx phoneme dictionary file: \"{}\"".format(phoneme_dictionary_file))
+
+        # create decoder object
+        config = pocketsphinx.Decoder.default_config()
+        # set the path of the hidden Markov model (HMM) parameter files
+        config.set_string("-hmm", acoustic_parameters_directory)
+        config.set_string("-lm", language_model_file)
+        config.set_string("-dict", phoneme_dictionary_file)
+        # disable logging (logging causes unwanted output in terminal)
+        config.set_string("-logfn", os.devnull)
+        decoder = pocketsphinx.Decoder(config)
+
+        return audio_data_path, decoder
+
+
+if __name__ == "__main__":
+    LipsyncGenerator().generate_timestamp_script_list()
