3a4d04257ba8bbab11523893595029388efca32f
Preethi Vaidyanathan <preethi@Preethis-MacBook-Pro.local>
2017-03-01 22:07:29 -0500

    Adding ability to write to a file.

--- src/lipsync_generator.py
+++ src/lipsync_generator.py
@@ -1,5 +1,6 @@
 import os
 import json
+import math
 from lipsync_generator_setup import LipsyncGeneratorSetup
 
 
@@ -20,20 +21,26 @@ class LipsyncGenerator:
 
         LipsyncGeneratorSetup.perform_speech_recognition(decoder, raw_data, keyword_entries)
 
-        script_data = [(seg.word,
-                        seg.start_frame,
-                        seg.end_frame,
-                        decoder.lookup_word(seg.word))
+        script_data = [{"full_word": seg.word,
+                        "start_time_s": float(seg.start_frame) / 100,
+                        "end_time_s": float(seg.end_frame) / 100,
+                        "phonemes": decoder.lookup_word(seg.word).split(" ")}
                        for seg in decoder.seg()]
         line_to_print = ''
         for word in script_data:
-            line_to_print = line_to_print + word[0] + ' '
+            line_to_print = line_to_print + word["full_word"] + ' '
 
         print('Words I heard: ', line_to_print)
 
         self.replace_phonemes_via_mapping(script_data)
+        self.convert_times_to_frame_rates(script_data)
+        output_filename = "test_file"
+        self.write_phonemes_to_file(script_data, output_filename)
 
-    def replace_phonemes_via_mapping(self, script_data):
+        dummy=5
+
+    @staticmethod
+    def replace_phonemes_via_mapping(script_data):
 
         # Read the info from the json file into the phoneme_mapping dictionary
         phoneme_mapping_json_path = os.path.join(os.getcwd() + "/../data", "phoneme_mapping.json")
@@ -44,11 +51,28 @@ class LipsyncGenerator:
             phoneme_mapping = json.load(phoneme_mapping_json)
 
         for script_data_entry in script_data:
-            phoneme_list = script_data_entry[3].split(" ")
-            phoneme_list = [phoneme_mapping[phoneme] for phoneme in phoneme_list]
+            script_data_entry["phonemes"] = [phoneme_mapping[phoneme] for phoneme in script_data_entry["phonemes"]]
 
+    @staticmethod
+    def convert_times_to_frame_rates(script_data):
+        # frames per second
+        frame_rate = 24.0
+        one_frame_seconds = 1 / frame_rate
+        for script_data_entry in script_data:
+            script_data_entry["start_frame"] = math.ceil(script_data_entry["start_time_s"] / one_frame_seconds)
+            script_data_entry["end_frame"] = math.ceil(script_data_entry["end_time_s"] / one_frame_seconds)
+
+    @staticmethod
+    def write_phonemes_to_file(script_data, output_filename):
+        output_filepath = os.path.join(os.getcwd(), "../data" + output_filename + ".dat")
+        if os.path.exists(output_filepath):
+            # Write to that file
+            file = open(output_filepath, 'r')
+            dummy=4
+        else:
+            # Create and write to that file
+            file = open(output_filepath, 'w')
             dummy=5
-
 
 
 if __name__ == "__main__":
