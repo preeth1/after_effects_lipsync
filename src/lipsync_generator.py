9e6e5400488da56e5e5d4eb9a9c4f3a0587ecbf1
Preethi Vaidyanathan <preethi@Preethis-MacBook-Pro.local>
2017-03-04 13:59:54 -0500

    changed dict to orderedDict so the .dat file prints in the correct order.

--- src/lipsync_generator.py
+++ src/lipsync_generator.py
@@ -1,6 +1,7 @@
 import os
 import json
 import math
+import collections
 from lipsync_generator_setup import LipsyncGeneratorSetup
 
 
@@ -12,14 +13,12 @@ class LipsyncGenerator:
 
         audio_file_name = 'TRLT.wav'
         output_filename = "TRLT"
+        frame_rate = 24.0
 
         # explicitly specified set of keywords
         keyword_entries = None
-
         audio_data_path, decoder = LipsyncGeneratorSetup.setup_decoder(audio_file_name, keyword_entries)
-
         raw_data = LipsyncGeneratorSetup.generate_raw_audio_data(audio_data_path)
-
         LipsyncGeneratorSetup.perform_speech_recognition(decoder, raw_data, keyword_entries)
 
         script_data = [{"full_word": seg.word,
@@ -34,10 +33,9 @@ class LipsyncGenerator:
         print('Words I heard: ', line_to_print)
 
         self.replace_phonemes_via_mapping(script_data)
-        self.convert_times_to_frame_rates(script_data)
+        self.convert_times_to_frame_rates(script_data, frame_rate)
         output_phoneme_dictionary = self.create_output_dictionary(script_data)
         self.write_phonemes_to_file(output_phoneme_dictionary, output_filename)
-        dummy=5
 
     @staticmethod
     def replace_phonemes_via_mapping(script_data):
@@ -54,17 +52,16 @@ class LipsyncGenerator:
             script_data_entry["phonemes"] = [phoneme_mapping[phoneme] for phoneme in script_data_entry["phonemes"]]
 
     @staticmethod
-    def convert_times_to_frame_rates(script_data):
-        # frames per second
-        frame_rate = 24.0
+    def convert_times_to_frame_rates(script_data, frame_rate):
         one_frame_seconds = 1 / frame_rate
         for script_data_entry in script_data:
-            script_data_entry["start_frame"] = int(math.ceil(script_data_entry["start_time_s"] / one_frame_seconds))
-            script_data_entry["end_frame"] = int(math.ceil(script_data_entry["end_time_s"] / one_frame_seconds))
+            script_data_entry["start_frame"] = int(script_data_entry["start_time_s"] / one_frame_seconds)
+            script_data_entry["end_frame"] = int(script_data_entry["end_time_s"] / one_frame_seconds)
 
     @staticmethod
     def create_output_dictionary(script_data):
-        output_phoneme_dictionary = {}
+        # Set the first frame to always start out neutral
+        output_phoneme_dictionary = collections.OrderedDict({-1: 'neutral'})
         for data_entry in script_data:
             start_frame = data_entry["start_frame"]
             end_frame = data_entry["end_frame"]
